b62a30f7f09c956ba1e12b8f388d69808f50fccb
liuzy2020 <liuzy2013@163.com>
2020-12-06 10:35:25 +0800

    开始编写综合测算的构件。

---
+++ src/calculate.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   calculate.py
+@Time    :   2020/12/06 09:58:18
+@Author  :   liuzy2020 
+@Version :   1.0
+@Contact :   liuzy2013@163.com
+@WebSite :   https://github.com/path2019
+'''
+# Start typing your code from here
+# 计算各项财务边界面
+
+from finance import Finance
+
+# 计算满足特定收益条件下的电价（含税）临界面
