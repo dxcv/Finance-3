d114d0a891ff23b57e238da7d07fdab89345cdda
liuzy2020 <liuzy2013@163.com>
2020-12-08 10:29:10 +0800

    完成对表格写入程序的改造

--- src/tools.py
+++ src/tools.py
@@ -15,7 +15,7 @@ import xlsxwriter as xlsw
 import numpy as np
 
 
-def write_excel(data, file='result.xlsx'):
+def write_excel(data, sheet_name=[], row_header=[], column_header=[], file='result.xlsx'):
     """
     将 data 列表数据写入到 file（excel）文件中。
 
@@ -24,7 +24,16 @@ def write_excel(data, file='result.xlsx'):
         data: list
             列表数据，可以是一~三维，若为一维、二维数据，则直接写入表格中；
                 若为三维数据，则第一维设置为表签名（SheetName）
+
+        sheet_name: list<str>, default = ['data']
+            表单标签，一维列表，数量应与 data 表单数对应，默认值为 ['data']
         
+        row_header: list<str>, default = []
+            数据行名，一维列表，数量应与 data 表单行数对应，默认值为 []
+
+        column_header: list<str>, default = []
+            数据列名，一维列表，数量应与 data 表单列数对应，默认值为 [] 
+
         file: str, default = 'result.xlsx'
             写入文件的名称，需包含后缀".xlsx"，默认值为 "result.xlsx"
 
@@ -45,31 +54,60 @@ def write_excel(data, file='result.xlsx'):
     # 根据不同维度进行表格写入
     try:
         workbook = xlsw.Workbook(file)  # 创建表格缓冲
-        if dim == 1:
-            sheet = workbook.add_worksheet("Data Result")
+        if dim == 1:  # 若为一维列表
+            if sheet_name:
+                sheet = workbook.add_worksheet(sheet_name[0])
+            else:
+                sheet = workbook.add_worksheet('Data')
+            if row_header:
+                sheet.write(1, 0, row_header[0])  # 写入行名
+            else:
+                sheet.write(1, 0, '数据')
             for index in range(len(data)):
-                sheet.write(0, index, data[index])
-        elif dim == 2:
-            sheet = workbook.add_worksheet("Data_Result")
+                if column_header:
+                    sheet.write(0, index + 1, column_header[index])  # 写入列名
+                else:
+                    sheet.write(0, index + 1, str(index + 1))
+                sheet.write(1, index + 1, data[index])  # 写入数据
+        elif dim == 2:  # 若为二维列表
+            if sheet_name:
+                sheet = workbook.add_worksheet(sheet_name[0])
+            else:
+                sheet = workbook.add_worksheet('Data')
             for i in range(len(data)):
+                if row_header:
+                    sheet.write(i + 1, 0, row_header[i])  # 写入行名
+                else:
+                    sheet.write(i + 1, 0, str(i + 1))
                 for j in range(len(data[i])):
-                    sheet.write(i, j, data[i][j])
+                    if i == 0:
+                        if column_header:
+                            sheet.write(i, j + 1, column_header[j])
+                        else:
+                            sheet.write(i, j + 1, str(j + 1))
+                        sheet.write(i + 1, j + 1, data[i][j])
+                    else:
+                        sheet.write(i + 1, j + 1, data[i][j])
         else:
             for k in range(len(data)):
-                # sheet = workbook.add_worksheet('表格 ' + str(k + 1))
-                ## 下面一行代码只是目前特殊目的需要，过了这次测算，即用上面一行代替这行
-                sheet = workbook.add_worksheet(str((k + 1) * 5) + ' 万 kW')
-                for i in range(len(data[k]) + 1):
-                    for j in range(len(data[k][i - 1]) + 1):
-                        ## 下面这行也是此次特殊测算的需要，过后删除或替换其它表头和列头
-                        if i == 0 and j == 0:
-                           sheet.write(i, j, '千瓦造价/上网小时') 
-                        elif i == 0 and j > 0:
-                            sheet.write(i, j, 4950 + j * 50)
-                        elif j == 0 and i > 0:
-                            sheet.write(i, j, 1750 + i * 50)
+                if sheet_name:
+                    sheet = workbook.add_worksheet(sheet_name[k])  # 表单标签
+                else:
+                    sheet = workbook.add_worksheet('表格' + str(k + 1))  # 表单标签
+                for i in range(len(data[k])):
+                    if row_header:
+                        sheet.write(i + 1, 0, row_header[i])  # 写入行名
+                    else:
+                        sheet.write(i + 1, 0, str(i + 1))
+                    for j in range(len(data[k][i])):
+                        if i == 0:
+                            if column_header:
+                                sheet.write(i, j + 1, column_header[j])
+                            else:
+                                sheet.write(i, j + 1, str(j + 1))
+                            sheet.write(i + 1, j + 1, data[k][i][j])
                         else:
-                            sheet.write(i, j, data[k][i-1][j-1])
+                            sheet.write(i + 1, j + 1, data[k][i][j])
         workbook.close()
         return 0
     except Exception as inst:
@@ -83,6 +121,6 @@ if __name__ == "__main__":
     data_2 = [data_1, data_1]
     data_3 = [data_2, data_2, data_2]
     
-    write_excel(data_1, 'result_1.xlsx')
-    write_excel(data_2, 'result_2.xlsx')
-    write_excel(data_3, 'result_3.xlsx')
+    write_excel(data_1, file='result_1.xlsx')
+    write_excel(data_2, file='result_2.xlsx')
+    write_excel(data_3, file='result_3.xlsx')
